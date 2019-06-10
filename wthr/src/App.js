0788bd7ecc5cba66220a0646d986a0c71bee3ae2
azurast <azstima@gmail.com>
2019-06-10 19:25:44 +0700

    New Components + Update App js
    introduced 3 new components + major changes in App.js to use with the API

--- wthr/src/App.js
+++ wthr/src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
 
+/* Import Components */
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+/* openweathermap.org API Key */
+const API_KEY = "2346a7254155ff5fd112d0fbc0fbfc85";
+
 class App extends React.Component{
+    getWeather = async (e) => {
+        //prevent default behaviour of the component when we press the button
+        e.preventDefault();
+        //user inputs
+        //const city = e.target.elements.name.city.value;
+        //const country = e.target.elements.name.country.value;
+        //the constant to fetch the specified url
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}`);
+        //convert the response to json format
+        const data = await api_call.json();
+        console.log("test");
+        console.log(data);
+    }
     render(){
         return(
-
-        )
+            <Titles/>
+            <Form getWeather={this.getWeather}/>
+            <Weather/>
+            </div>
+        );
     }
 }
 
